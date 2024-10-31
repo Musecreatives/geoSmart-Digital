2dcbe553dc9cb8fe7d1369575eb658ebe1b3351a
iamdestinychild <omoruyidestiny30@gmail.com>
2024-10-31 18:37:42 +0100

    module misspelling

--- src/app/success/page.jsx
+++ src/app/success/page.jsx
@@ -3,7 +3,7 @@
 
 import { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
-import styles from "./Success.module.css";
+import styles from "./success.module.css";
 
 const SuccessPage = () => {
   const searchParams = useSearchParams();
